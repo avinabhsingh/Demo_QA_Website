8894b56c9e614f4ea47edcf7f58551e38292b1c8
singhavinabh <119515463+singhavinabh@users.noreply.github.com>
2024-09-02 22:43:45 +0530

    Updated Code

--- cypress/pageObjects/Book Store Application/Profile.js
+++
@@ -1,8 +0,0 @@
-class profile {
-    elements = {
-        username_label: () => cy.get('[id="userName-label"]'),
-        username_value: () => cy.get('[id="userName-value"]'),
-
-    }
-}
-export default new profile;
